e3ade4332e0232b0b67bf5262dc114190ccf2acc
David Malviya <145090201+david-malviya@users.noreply.github.com>
2025-07-04 02:33:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('RGB')
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     processed = preprocess_image(image)
     prediction = model.predict(processed)
